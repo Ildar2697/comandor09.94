7f4141c77b3fac6d18539c68795245d33961f642
ILDAR <sirazhitdinov2013@mail.ru>
2021-04-11 20:45:05 +0300

    Remade hero-block

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,11 +8,28 @@ function init(){
         // Порядок по умолчанию: «широта, долгота».
         // Чтобы не определять координаты центра карты вручную,
         // воспользуйтесь инструментом Определение координат.
-        center: [55.76, 37.64],
+        center: [54.731531, 55.955309],
         // Уровень масштабирования. Допустимые значения:
         // от 0 (весь мир) до 19.
-        zoom: 7
+        zoom: 11
     });
+    var myPlacemark1 = new ymaps.Placemark([54.717438, 55.954065], {}, {
+      iconLayout: 'default#image',
+    });
+    var myPlacemark2 = new ymaps.Placemark([54.711775, 55.985632], {}, {
+      iconLayout: 'default#image',
+    });
+    var myPlacemark3 = new ymaps.Placemark([54.781316, 56.032820], {}, {
+      iconLayout: 'default#image',
+    });
+    var myPlacemark4 = new ymaps.Placemark([54.760683, 56.030889], {}, {
+      iconLayout: 'default#image',
+    });
+    myMap.geoObjects.add(myPlacemark1);
+    myMap.geoObjects.add(myPlacemark2);
+    myMap.geoObjects.add(myPlacemark3);
+    myMap.geoObjects.add(myPlacemark4);
+  
 }
 const defaultSelect = () => {
     const element = document.querySelector('.callback__select');
@@ -27,26 +44,36 @@ let callbackInputTel = document.querySelector('.callback__form-tel');
 let inputMask = new Inputmask('+7 (999) 999-99-99');
 inputMask.mask(callbackInputTel);
 
-const swiperHero1 = new Swiper('.swiper-container-hero1', {
-    // Optional parameters
+// const swiperHero1 = new Swiper('.swiper-container-hero1', {
+//     direction: 'horizontal',
+//     loop: false,
+  
+//     pagination: {
+//       el: '.swiper-pagination-hero1',
+//     },
+  
+//     navigation: {
+//       nextEl: '.swiper-button-next-hero1',
+//       prevEl: '.swiper-button-prev-hero1',
+//     },
+//   });
+
+const swiperHero2 = new Swiper('.swiper-container-hero2', {
     direction: 'horizontal',
     loop: false,
   
-    // If we need pagination
     pagination: {
-      el: '.swiper-pagination-hero1',
+      el: '.swiper-pagination',
     },
   
-    // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next-hero1',
-      prevEl: '.swiper-button-prev-hero1',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar-hero1',
-    // },
   });
+
+
+
   let upBtn = document.querySelector('.up-btn');
   upBtn.addEventListener('click', function() {
     window.scrollTo({
@@ -58,7 +85,7 @@ const swiperHero1 = new Swiper('.swiper-container-hero1', {
 
   window.addEventListener('scroll', function(event) {
     let scroll = this.scrollY;
-    console.log(scroll)
+    // console.log(scroll)
     if (scroll < 400) {
       upBtn.style.display = 'none';
     } else {
