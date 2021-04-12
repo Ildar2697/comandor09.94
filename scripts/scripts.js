88917673d7f98e95bfcdc93b6f849279772d7c4b
ILDAR <sirazhitdinov2013@mail.ru>
2021-04-12 05:01:07 +0300

    Intermediate on sliders

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,7 @@
-// Функция ymaps.ready() будет вызвана, когда
-// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
 ymaps.ready(init);
 function init(){
-    // Создание карты.
     var myMap = new ymaps.Map("map", {
-        // Координаты центра карты.
-        // Порядок по умолчанию: «широта, долгота».
-        // Чтобы не определять координаты центра карты вручную,
-        // воспользуйтесь инструментом Определение координат.
         center: [54.731531, 55.955309],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
         zoom: 11
     });
     var myPlacemark1 = new ymaps.Placemark([54.717438, 55.954065], {}, {
@@ -72,7 +63,34 @@ const swiperHero2 = new Swiper('.swiper-container-hero2', {
     },
   });
 
+  const Slider1 = new Swiper('.swiper-container-hero2', {
+    direction: 'horizontal',
+    loop: false,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 
+
+  const Slider2 = new Swiper('.sliders1-container', {
+    direction: 'horizontal',
+    loop: false,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 
   let upBtn = document.querySelector('.up-btn');
   upBtn.addEventListener('click', function() {
@@ -85,7 +103,6 @@ const swiperHero2 = new Swiper('.swiper-container-hero2', {
 
   window.addEventListener('scroll', function(event) {
     let scroll = this.scrollY;
-    // console.log(scroll)
     if (scroll < 400) {
       upBtn.style.display = 'none';
     } else {
@@ -104,4 +121,30 @@ const swiperHero2 = new Swiper('.swiper-container-hero2', {
         left: 0,
         behavior: 'smooth'
       })
-  }})
+  }});
+
+
+  let headerBurger = document.querySelector('.header__burger');
+  let menu = document.querySelector('.menu');
+  let menuCloseBtn = document.querySelector('.menu__close-btn');
+
+  headerBurger.addEventListener('click', function() {
+    menu.classList.toggle('menu-active');
+  });
+  menuCloseBtn.addEventListener('click', function() {
+    menu.classList.remove('menu-active');
+  });
+
+  let menuList = document.querySelector('.menu__list');
+  let menuItemLink = document.querySelectorAll('.menu__item-link');
+  let menuExpandedTitle = document.querySelector('.menu__expanded-title');
+
+  menuItemLink.forEach(function(el) {
+    el.addEventListener('click', function() {
+      menuList.style.display = 'none';
+      document.querySelector(this.hash).style.display = 'block';
+      menuExpandedTitle.innerHTML = this.innerHTML;
+      console.log(this.hash)
+      console.log(this.innerHTML);
+    })
+  })
